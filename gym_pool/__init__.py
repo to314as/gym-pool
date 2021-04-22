476061f432123c742e8156e4c8d3deb50af13db7
to314as <t.f.oberkofler@umail.leidenuniv.nl>
2021-04-22 23:15:37 +0200

    update structure

--- gym_pool/__init__.py
+++ gym_pool/__init__.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 register(
     id='Pool-v0',
-    entry_point='gym_soccer.envs:PoolEnv',
+    entry_point='gym_pool.envs:PoolEnv',
     timestep_limit=1000,
     reward_threshold=1.0,
     nondeterministic = True,
--- gym_pool/envs/__init__.py
+++ gym_pool/envs/__init__.py
@@ -1,3 +1,3 @@
-from gym_pool.envs.soccer_env import PoolEnv
+from gym_pool.envs.pool_env import PoolEnv
 #from gym_soccer.envs.soccer_empty_goal import SoccerEmptyGoalEnv
 #from gym_soccer.envs.soccer_against_keeper import SoccerAgainstKeeperEnv
