be0fc736c8760fd0bddff70fd005e2c78b07270d
to314as <t.f.oberkofler@umail.leidenuniv.nl>
2021-04-22 20:11:28 +0200

    update setup

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 setup(name='gym_pool',
       version='0.0.1',
+      url='https://github.com/to314as/gym-pool',
+      author='Tobias Oberkofler',
+      author_email='tobi20083@gmail.com',
+      py_modules=['gym-pool'],
       install_requires=['gym>=0.2.3',
                         'numpy',
                         'pygame',
                         'zope.event',
                         'torch',
-                        'torchvision'
-                        ]
+                        'torchvision']
 )
