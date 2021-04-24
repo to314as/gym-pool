6e2415d9937d04627fc3a4cd10ed5f943c6a30ff
to314as <t.f.oberkofler@umail.leidenuniv.nl>
2021-04-24 23:00:45 +0200

    Update __init__.py

--- gym_pool/envs/__init__.py
+++ gym_pool/envs/__init__.py
@@ -7,5 +7,4 @@ import gym_pool.envs.table_sprites
 import gym_pool.envs.collisions
 import gym_pool.envs.gamestate
 from gym_pool.envs.pool_env import PoolEnv
-#from gym_soccer.envs.soccer_empty_goal import SoccerEmptyGoalEnv
-#from gym_soccer.envs.soccer_against_keeper import SoccerAgainstKeeperEnv
+from gym_pool.envs.pool_env_3 import PoolEnv3
